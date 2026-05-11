000fa00729a4468b1fdbaaf91d937f28bc1adda3
feryts <81550388+feryts@users.noreply.github.com>
2026-05-11 09:34:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require("path");
 
 const app = express();
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(__dirname));
 
 const KOLAYGELSIN_KULLANICI = "seyhanbs";
 const KOLAYGELSIN_SIFRE     = "153759";
